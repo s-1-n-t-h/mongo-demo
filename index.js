cf5836adff0af78aca43829153605447d6e88e38
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-25 15:42:15 +0530

    pagination

--- index.js
+++ index.js
@@ -28,23 +28,12 @@ async function createCourse() {
 }
 
 async function getCourses() {
-	// comparision operators
-	// eq - equal to
-	// ne - not equal to
-	// gt - greater than
-	// gte - greater than or equal to
-	// lt - less than
-	// lte - less than or equal to
-	// in
-	// nin (not in)
-
-	// logical operators
-	// or
-	// and
-	// syntax = .find().or([{author: 'John'}, {isPublished: true}]).and()
+	const pageNumber = 2;
+    const pageSize = 10;
 	const courses = await Course
         .find({author: /^yaswanth/i , isPublished: true})
-		.limit(2) // limits no of records from search results
+        .skip((pageNumber-1)*pageSize)
+		.limit(pageSize) // limits no of records from search results
 		.sort({ tags: 1 }) // sorts the results by specified attribute
 		.select({ name: 1, tags: 1 }) // selects the given attributes from each record
         .count()
