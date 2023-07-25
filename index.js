ceb6525916a9d41153ff9b53ac7542ba557b3695
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-25 15:03:52 +0530

    retrieve documents from db

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-    .then(() => console.log('Connected to MongoDB....'))
-    .catch(err => console.error('Error connecting to MongoDB..',err))
+mongoose
+	.connect("mongodb://localhost/playground")
+	.then(() => console.log("Connected to MongoDB...."))
+	.catch((err) => console.error("Error connecting to MongoDB..", err));
 
 const courseSchema = mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [String],
-    date: { type: Date, default: Date.now},
-    isPublished: Boolean
-})
+	name: String,
+	author: String,
+	tags: [String],
+	date: { type: Date, default: Date.now },
+	isPublished: Boolean,
+});
 
-const Course = mongoose.model('Course', courseSchema)
+const Course = mongoose.model("Course", courseSchema);
 
+async function createCourse() {
+	const course = new Course({
+		name: "React Course",
+		author: "Yaswanth",
+		tags: ["frontend", "javascript"],
+		isPublished: true,
+	});
 
-async function createCourse(){
-    const course = new Course({
-        name: "React Course",
-        author: "Yaswanth",
-        tags: ["frontend", "javascript"],
-        isPublished: true,
-    },);
-
-    const result = await course.save();
-    console.log(result);
+	const result = await course.save();
+	console.log(result);
 }
 
-createCourse() 
+async function getCourses() {
+	const courses = await Course.find({ isPublished: true }) // to find records that hav a soecific attribute's value
+		.limit(2) // limits no of records from search results
+		.sort({ tags: 1 }) // sorts the results by specified attribute
+		.select({ name: 1, tags: 1 }); // selects the given attributes from each record
+	console.log(courses);
+}
+
+getCourses();
