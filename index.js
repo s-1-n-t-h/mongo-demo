284856c116abe1a8da365aaaae8cf4a56ba0973f
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-25 15:21:12 +0530

    logical operators

--- index.js
+++ index.js
@@ -28,7 +28,19 @@ async function createCourse() {
 }
 
 async function getCourses() {
-	const courses = await Course.find({ isPublished: true }) // to find records that hav a soecific attribute's value
+    // operators
+    // eq - equal to
+    // ne - not equal to
+    // gt - greater than
+    // gte - greater than or equal to
+    // lt - less than
+    // lte - less than or equal to
+    // in 
+    // nin (not in)
+	const courses = await Course
+        // .find({ isPublished: true }) // to find records that hav a soecific attribute's value
+        //.find({price: { $gt : 10, $lt: 20}}) // less than 20 and greater than 10 using operators
+        .find({price: { $in : [10,15,20]}}) // prices equal to 10 / 15/ 20
 		.limit(2) // limits no of records from search results
 		.sort({ tags: 1 }) // sorts the results by specified attribute
 		.select({ name: 1, tags: 1 }); // selects the given attributes from each record
